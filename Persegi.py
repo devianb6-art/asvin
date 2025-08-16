25d86fef83b964c86dcb5c7c4cafce9e0422c42c
devianb6-art <devianb6@gmail.com>
2025-08-16 16:44:00 +0800

    Add files via upload

---
+++ Persegi.py
@@ -0,0 +1,15 @@
+def hitung_persegi():
+    try:
+        sisi = float(input("Masukkan panjang sisi persegi: "))
+        if sisi <= 0:
+            print("Panjang sisi tidak valid.")
+        else:
+            luas = sisi ** 2
+            keliling = 4 * sisi
+            print(f"Luas persegi adalah {luas}")
+            print(f"Keliling persegi adalah {keliling}")
+    except ValueError:
+        print("Input tidak valid. Harap masukkan angka.")
+
+# Memanggil fungsi
+hitung_persegi()
